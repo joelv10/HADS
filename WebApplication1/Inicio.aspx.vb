a52cc56060fed7634418328a581ccda2fd6085a3
joelv10 <33729604+joelv10@users.noreply.github.com>
2019-04-07 18:52:24 +0200

    Add files via upload

--- WebApplication1/Inicio.aspx.vb
+++ WebApplication1/Inicio.aspx.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Data.SqlClient
+Imports System.Security.Cryptography
 
 Public Class Inicio
     Inherits System.Web.UI.Page
@@ -6,21 +7,33 @@ Public Class Inicio
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
-
+    Private Function encriptarcontra(ByVal contra As String) As String
+        Dim pass As New System.Security.Cryptography.SHA256CryptoServiceProvider
+        Dim pass2 As Byte() = pass.ComputeHash(Encoding.UTF8.GetBytes(contra))
+        Dim sBuilder As New StringBuilder()
+        Dim cont As Integer
+        For cont = 0 To pass2.Length - 1
+            sBuilder.Append(pass2(cont).ToString("x2"))
+        Next cont
+        Return sBuilder.ToString
+    End Function
     Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim accesoBD As New ClassLibrary1.AccesodatosSQL
         accesoBD.Conectar()
-        If (accesoBD.contar(TextBox1.Text, TextBox2.Text) = 1) Then
-            If (accesoBD.estaConfirmado(TextBox1.Text, TextBox2.Text)) Then
+
+        If (accesoBD.contar(TextBox1.Text, encriptarcontra(TextBox2.Text)) = 1) Then
+            If (accesoBD.estaConfirmado(TextBox1.Text, encriptarcontra(TextBox2.Text))) Then
                 If (accesoBD.esAlumno(TextBox1.Text)) Then
+                    System.Web.Security.FormsAuthentication.SetAuthCookie("alumno", False)
                     Response.Redirect("http://hads18-villalobos.azurewebsites.net/Alumno.aspx?user=" & TextBox1.Text)
                 Else
+                    System.Web.Security.FormsAuthentication.SetAuthCookie("profesor", False)
                     Response.Redirect("http://hads18-villalobos.azurewebsites.net/Profesor.aspx?user=" & TextBox1.Text)
                 End If
 
             Else
                 Label1.Text = "El usuario no ha sido verificado aún, por favor, consulte el mensaje de su email"
-                    Label1.Visible = True
+                Label1.Visible = True
             End If
         Else
             Label1.Text = "Usuario/Password incorrecto/s"
