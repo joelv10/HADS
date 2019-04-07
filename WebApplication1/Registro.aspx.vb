c2fd110406227064d596821117892c4054571995
joelv10 <joelfutbol5@gmail.com>
2019-04-07 22:47:32 +0200

    lab de seguridad

--- WebApplication1/Registro.aspx.vb
+++ WebApplication1/Registro.aspx.vb
@@ -1,4 +1,4 @@
-﻿Imports AccesoDatos.accesodatosSQL
+﻿Imports AccesoDatos.accesodatosSQL, System.Security.Cryptography
 Public Class Registro
     Inherits System.Web.UI.Page
 
@@ -8,19 +8,23 @@ Public Class Registro
     Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
         Try
+            Dim encriptar As SHA256
             Dim enviar As New ClassLibrary2.EnviarEmails
             Dim numRandom = enviar.crearRandom
             Dim email = TextBox1.Text
             Dim nombre = TextBox2.Text
             Dim apellido = TextBox5.Text
             Dim pass = TextBox3.Text
+            Dim pass2 As Byte()
+            encriptar = SHA256.Create()
+            pass2 = encriptar.ComputeHash(System.Text.Encoding.ASCII.GetBytes(pass))
             Dim tipo = DropDownList1.Text
             Dim accesoBD As New ClassLibrary1.AccesodatosSQL
             accesoBD.Conectar()
             If (accesoBD.buscarUsuario(email) = False) Then
                 Label7.Text = "Por favor, verifique su registro mediante el email que le acabamos de mandar, gracias "
                 Label7.Visible = True
-                accesoBD.insertar(email, nombre, apellido, numRandom, False, tipo, pass)
+                accesoBD.insertar(email, nombre, apellido, numRandom, False, tipo, System.Text.Encoding.Unicode.GetString(pass2))
                 enviar.enviarEmail(email, "Registro", "Para continuar su registro haga click aquí http://hads18-villalobos.azurewebsites.net/Confirmar.aspx?numero=" & numRandom & "&usuario=" & email, numRandom)
                 accesoBD.cerrarconexion()
             Else
--- WebApplication2/Profesor.aspx.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class Profesor
-    Inherits System.Web.UI.Page
-
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        Session("usuario") = Request.QueryString("user")
-    End Sub
-
-End Class
